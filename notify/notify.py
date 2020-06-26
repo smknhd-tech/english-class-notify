ef6266cfd6e4bbcbf0a3a01b88dae56cea143e2e
KAZY <kwacut@gmail.com>
2020-06-27 00:34:40 +0900

    コメントアウトを英語化

--- notify/notify.py
+++ notify/notify.py
@@ -6,23 +6,21 @@ from datetime import datetime, date, timedelta
 
 
 def PythonNotify(message, token, img_path=""):
-    # 諸々の設定
     line_notify_api = "https://notify-api.line.me/api/notify"
     line_notify_token = token
     headers = {"Authorization": "Bearer " + line_notify_token}
-    # メッセージ
     payload = {"message": message}
-    # 画像を含むか否か
+    # Whether the image is included or not
     if img_path == "":
         res = requests.post(line_notify_api, data=payload, headers=headers)
     else:
-        # 画像
+        # image
         files = {"imageFile": open(img_path, "rb")}
         res = requests.post(line_notify_api, data=payload, headers=headers, files=files)
     return res
 
 
-# 最大表示数を20としたのは20以上のクラスが表示されると通知が長くなりすぎると思ったため
+# The reason why I set the maximum number of displays to 20 is because I thought the notification would be too long if more than 20 classes were displayed.
 def main(day, max_lessons=20, config_file="conf/config.txt"):
     config = configparser.ConfigParser()
     config.read(config_file)
@@ -32,15 +30,15 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     USER_NAME = config["USER INFO"]["NAME"]
     TUTORS_NAME_ID_MAP = config["TUTORS"]
     CONTENT_REC_PATH = f"/tmp/app/db/instead-db-tmp-for-{USER_NAME}.txt"
-    # today tomorrw といった引数を許容させたのは今日から何日後という意味で数字を引数をすると使いにくと考えたため
-    # 数値変換しているのはtimedeltaを用いて日付の演算を行うため
+    # The reason we allow arguments like today tomorrw is that we thought it would be difficult to use a number as an argument to say how many days from today.
+    # The reason for the numerical conversion is to use timedelta to calculate the date
     if day == "" or day == "today":
         days = 0
     elif day == "tomorrow":
         days = 1
     else:
         days = int(day)
-    # 過去は予約不可能であるため 今日から何日後 を指定させるのが使い勝手が良いかなと思ったため
+    # Since the past is unavailable, I thought it would be more convenient to specify the number of days from today
     date = datetime.today() + timedelta(days=days)
     date = datetime.strftime(date, "%Y-%m-%d")
     logger.info("Search date: %s", date)
@@ -56,8 +54,8 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     logger.info("前回のメッセージ: %s", file_content)
     for name, id in TUTORS_NAME_ID_MAP.items():
         res = requests.get(f"{TUTORS_URL}{id}")
-        # 予約ページへのリンクを予約可能の判定条件としているため
-        # パターンに日付を加えているのは対象としている日のみを抽出するため
+        # Because the link to the reservation page is a condition for determining whether a reservation is possible.
+        # The date is added to the pattern in order to extract only the dates we are interested in.
         yoyakuka = re.compile(fr"{date}.*?予約可</a>")
         yoyakuka_lessons = yoyakuka.findall(res.text)
 
@@ -77,9 +75,9 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     if message != file_content:
         with open(CONTENT_REC_PATH, mode="w") as fw:
             fw.write(message)
-        # 大文字変換はtoday tomorrowよりTODAY TOMORROWのほうが見やすいかなと思ったため
+        # I thought it would be easier to see the capitalization conversions in TODAY TOMORROW than in today tomorrow
         res = PythonNotify(f"@{USER_NAME}" + message + day.upper(), TOKEN)
-        # HTTP レスポンスステータスコードをログとして残しておくと障害対応が行いやすくなるため
+        # The HTTP response status code is kept in a log to make it easier to respond to problems.
         logger.info("LINE通知:%s", res)
 
 
