80b089ac1dd707d3ca38830086e38c9b571ff53e
KAZY <kwacut@gmail.com>
2020-07-10 08:31:30 +0900

    端的な英語に変更(脱DeepL)

--- notify/notify.py
+++ notify/notify.py
@@ -10,17 +10,15 @@ def PythonNotify(message, token, img_path=""):
     line_notify_token = token
     headers = {"Authorization": "Bearer " + line_notify_token}
     payload = {"message": message}
-    # Whether the image is included or not
     if img_path == "":
         res = requests.post(line_notify_api, data=payload, headers=headers)
     else:
-        # image
         files = {"imageFile": open(img_path, "rb")}
         res = requests.post(line_notify_api, data=payload, headers=headers, files=files)
     return res
 
 
-# The reason why I set the maximum number of displays to 20 is because I thought the notification would be too long if more than 20 classes were displayed.
+# Because display more than 20 lessons is too much
 def main(day, max_lessons=20, config_file="conf/config.txt"):
     config = configparser.ConfigParser()
     config.read(config_file)
@@ -30,19 +28,20 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     USER_NAME = config["USER INFO"]["NAME"]
     TUTORS_NAME_ID_MAP = config["TUTORS"]
     CONTENT_REC_PATH = f"/tmp/app/db/instead-db-tmp-for-{USER_NAME}.txt"
-    # The reason we allow arguments like today tomorrw is that we thought it would be difficult to use a number as an argument to say how many days from today.
     # The reason for the numerical conversion is to use timedelta to calculate the date
+    # Because Using "today" "tomorrow" as argument is more understandable than 0,1
     if day == "" or day == "today":
         days = 0
     elif day == "tomorrow":
         days = 1
     else:
         days = int(day)
-    # Since the past is unavailable, I thought it would be more convenient to specify the number of days from today
+    # date is set the number of days from today
     date = datetime.today() + timedelta(days=days)
     date = datetime.strftime(date, "%Y-%m-%d")
     logger.info("Search date: %s", date)
-    date_re = re.compile(fr"[0-9]{{2}}:[0-9]{{2}}")
+    # to extract lesson time
+    time_re = re.compile(fr"[0-9]{{2}}:[0-9]{{2}}")
     message = ""
     try:
         with open(CONTENT_REC_PATH, mode="x") as f:
@@ -54,8 +53,7 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     logger.info("前回のメッセージ: %s", file_content)
     for name, id in TUTORS_NAME_ID_MAP.items():
         res = requests.get(f"{TUTORS_URL}{id}")
-        # Because the link to the reservation page is a condition for determining whether a reservation is possible.
-        # The date is added to the pattern in order to extract only the dates we are interested in.
+        # Date and word "予約可" are the conditions for reservation is possible
         yoyakuka = re.compile(fr"{date}.*?予約可</a>")
         yoyakuka_lessons = yoyakuka.findall(res.text)
 
@@ -64,9 +62,9 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
 
         lessons = ", ".join(
             [
-                f"{date_re.search(lesson).group()}"
+                f"{time_re.search(lesson).group()}"
                 for lesson in yoyakuka_lessons[:max_lessons]
-                if date_re.search(lesson)
+                if time_re.search(lesson)
             ]
         )
         message += f"\n{TUTORS_URL}{id} \n{lessons} \n"
@@ -75,9 +73,9 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     if message != file_content:
         with open(CONTENT_REC_PATH, mode="w") as fw:
             fw.write(message)
-        # I thought it would be easier to see the capitalization conversions in TODAY TOMORROW than in today tomorrow
+        # the upper case "TODAY","TOMORROW" looks better than the lower case "today", "tomorrow"
         res = PythonNotify(f"@{USER_NAME}" + message + day.upper(), TOKEN)
-        # The HTTP response status code is kept in a log to make it easier to respond to problems.
+        # The HTTP response status code will be good a log to fix problem
         logger.info("LINE通知:%s", res)
 
 
