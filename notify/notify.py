720e30f34269735f2655341d011396643388a670
KAZY <kwacut@gmail.com>
2020-07-18 14:11:33 +0900

    お気持ちを述べた (#27)
    * お気持ちを述べた
    
    * コメントアウトを英語化
    
    * 端的な英語に変更(脱DeepL)
    
    * notify関数の画像送信部分にコメント追加

--- notify/notify.py
+++ notify/notify.py
@@ -6,22 +6,20 @@ from datetime import datetime, date, timedelta
 
 
 def PythonNotify(message, token, img_path=""):
-    # 諸々の設定
     line_notify_api = "https://notify-api.line.me/api/notify"
     line_notify_token = token
     headers = {"Authorization": "Bearer " + line_notify_token}
-    # メッセージ
     payload = {"message": message}
-    # 画像を含むか否か
     if img_path == "":
         res = requests.post(line_notify_api, data=payload, headers=headers)
     else:
-        # 画像
+        # TODO: This is used to send the analized image.
         files = {"imageFile": open(img_path, "rb")}
         res = requests.post(line_notify_api, data=payload, headers=headers, files=files)
     return res
 
 
+# Because display more than 20 lessons is too much
 def main(day, max_lessons=20, config_file="conf/config.txt"):
     config = configparser.ConfigParser()
     config.read(config_file)
@@ -31,17 +29,20 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     USER_NAME = config["USER INFO"]["NAME"]
     TUTORS_NAME_ID_MAP = config["TUTORS"]
     CONTENT_REC_PATH = f"/tmp/app/db/instead-db-tmp-for-{USER_NAME}.txt"
-
+    # The reason for the numerical conversion is to use timedelta to calculate the date
+    # Because Using "today" "tomorrow" as argument is more understandable than 0,1
     if day == "" or day == "today":
         days = 0
     elif day == "tomorrow":
         days = 1
     else:
         days = int(day)
+    # date is set the number of days from today
     date = datetime.today() + timedelta(days=days)
     date = datetime.strftime(date, "%Y-%m-%d")
     logger.info("Search date: %s", date)
-    date_re = re.compile(fr"[0-9]{{2}}:[0-9]{{2}}")
+    # to extract lesson time
+    time_re = re.compile(fr"[0-9]{{2}}:[0-9]{{2}}")
     message = ""
     try:
         with open(CONTENT_REC_PATH, mode="x") as f:
@@ -53,6 +54,7 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     logger.info("前回のメッセージ: %s", file_content)
     for name, id in TUTORS_NAME_ID_MAP.items():
         res = requests.get(f"{TUTORS_URL}{id}")
+        # Date and word "予約可" are the conditions for reservation is possible
         yoyakuka = re.compile(fr"{date}.*?予約可</a>")
         yoyakuka_lessons = yoyakuka.findall(res.text)
 
@@ -61,9 +63,9 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
 
         lessons = ", ".join(
             [
-                f"{date_re.search(lesson).group()}"
+                f"{time_re.search(lesson).group()}"
                 for lesson in yoyakuka_lessons[:max_lessons]
-                if date_re.search(lesson)
+                if time_re.search(lesson)
             ]
         )
         message += f"\n{TUTORS_URL}{id} \n{lessons} \n"
@@ -74,7 +76,9 @@ def main(day, max_lessons=20, config_file="conf/config.txt"):
     if message != file_content:
         with open(CONTENT_REC_PATH, mode="w") as fw:
             fw.write(message)
+        # the upper case "TODAY","TOMORROW" looks better than the lower case "today", "tomorrow"
         res = PythonNotify(f"@{USER_NAME}" + message + day.upper(), TOKEN)
+        # The HTTP response status code will be good a log to fix problem
         logger.info("LINE通知:%s", res)
 
 
