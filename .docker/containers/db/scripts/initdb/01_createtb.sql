0bb50a2ea45dbb2000efa5040192ba48ea44e72c
KAZY <kazypinksaurus@gmail.com>
2020-11-14 19:20:45 +0900

    2 tables added

--- .docker/containers/db/scripts/initdb/01_createtb.sql
+++ .docker/containers/db/scripts/initdb/01_createtb.sql
@@ -4,3 +4,16 @@ CREATE TABLE submit_messages (
   content_of_message text NOT NULL,
   submitted_time timestamp NOT NULL
 );
+
+CREATE TABLE teachers(
+  id varchar(6) NOT NULL primary key,
+  name varchar(30) NOT NULL,
+  country varchar(40) NOT NULL
+);
+
+CREATE TABLE teacher_schedules(
+  teacher_id varchar(6) NOT NULL,
+  slot_datetime timestamp NOT NULL,
+  opened_datetime timestamp NOT NULL,
+  primary key(teacher_id, slot_datetime)
+);
