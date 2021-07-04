4db7de27990c69cc2b56e61b5cde8b5eee9881ef
KAZY <kazypinksaurus@gmail.com>
2021-07-04 12:47:59 +0900

    2つのテーブルを追加しました(#9) (#40)
    * 2 tables added
    
    * add new line at end of file
    
    * revise table

--- .docker/containers/db/scripts/initdb/01_createtb.sql
+++ .docker/containers/db/scripts/initdb/01_createtb.sql
@@ -4,3 +4,26 @@ CREATE TABLE submit_messages (
   content_of_message text NOT NULL,
   submitted_time timestamp NOT NULL
 );
+
+CREATE TABLE teachers(
+  id varchar(6) NOT NULL primary key,
+  name varchar(30) NOT NULL,
+  country varchar(40) NOT NULL
+);
+
+CREATE TABLE teacher_events(
+  teacher_id varchar(6) NOT NULL,
+  slot_id integer NOT NULL,
+  event_id integer NOT NULL,
+  happend_on date NOT NULL
+);
+
+CREATE TABLE events(
+  id serial,
+  name text NOT NULL
+);
+
+CREATE TABLE slots(
+  id serial,
+  time text NOT NULL
+);
